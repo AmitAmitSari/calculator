b78d9be6c55790a4805b7d0e336017d216eff085
Amit Sari <amitsari@gmail.com>
2020-12-07 12:19:44 +0200

    Working number buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
 import React, { useState } from 'react'
 
+
+function Button(props) {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  );
+}
+
+
 function App() {
+  let [number, setNumber] = useState(null);
   return (
     <div>
+      <div>
+        {number}
+      </div>
+      {
+        Array(3).fill(null).map((_, i) => -i + 2).map(i => {
+          return (
+            <div>
+            {
+              Array(3).
+              fill(null).
+              map((_, j) => i * 3 + j + 1).
+              map(num =>
+                {return <Button text={num} onClick={() => setNumber(number * 10 + num)}></Button>})
+            }
+            </div>);
+          })
+      }
     </div>
   );
 }
