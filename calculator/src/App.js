5c6b641714bc8da4a9a20dc0dc9b3a73387a006a
Amit Sari <amitsari@gmail.com>
2020-12-07 11:53:12 +0200

    Clean slate

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
 import React, { useState } from 'react'
 
-
-function Button(props) {
-  return (
-    <button onClick={props.onClick}>{props.text}</button>
-  );
-}
-
-
 function App() {
-  let [text, setText] = useState('initial');
   return (
     <div>
-      <Button text={text} onClick={() => setText(text + '0')}></Button>
     </div>
   );
 }
